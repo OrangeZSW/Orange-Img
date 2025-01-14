8fc91c52385867e88e14cc5f95968667a6e64697
orange <zsw3166483606@163.com>
2025-01-14 17:42:42 +0800

    init

---
+++ backend/src/main/java/online/orange/img/ImgApplication.java
@@ -0,0 +1,15 @@
+package online.orange.img;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author orange
+ * @since 2025/1/14
+ */
+@SpringBootApplication
+public class ImgApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImgApplication.class, args);
+    }
+}
